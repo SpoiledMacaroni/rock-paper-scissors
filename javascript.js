72120313776d1ff1ee8818251636b3dfc0df6b4c
SpoiledMacaroni <60438998+SpoiledMacaroni@users.noreply.github.com>
2023-10-16 17:04:57 -0400

    Update Conditions and Add Game Function Updated Round functionality to include all 3 win conditions, added loop to repeat function call.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,32 @@
 console.log("Hello World!");
 
-let choices = ['Rock', 'Paper', 'Scissors'];
-
-let playerSelection = 'rock';
-let computerSelection = getComputerChoice();
+let choices = ['rock', 'paper', 'scissors'];
+let score = 0;
 
 function getComputerChoice() {
-    console.log(choices[Math.floor(Math.random() * choices.length)]);
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
+function round() {
+    let playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
+    let computerSelection = getComputerChoice();
+    console.log(getComputerChoice());
+    
+    if ((playerSelection === 'rock' && computerSelection ==='scissors')||(playerSelection === 'scissors' && computerSelection === 'paper')||(playerSelection === 'paper' && computerSelection === 'rock')) {
+        console.log("You Win!"), score++;
+        console.log(score)
+    } else if (playerSelection === computerSelection) {
+        console.log("Tie!")
+    }    else {
+        console.log('Keep working on it!');
+    }
+}
+
+function game() {
+while (score < 5) {
+    round();
+}
+}
+
+game()
 
